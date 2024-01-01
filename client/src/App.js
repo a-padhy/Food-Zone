95c8891b64ec57d64b23591e90ff262ffdfc1fa9
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2024-01-02 02:04:02 +0530

    client-setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
