72407bac08989027699bd9e255c6da7ce518826a
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2024-01-02 01:52:54 +0530

    server-authentication

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 import express from "express";
+import cors from "cors";
+import connectDB from "./db/connect.js";
+import { userRouter } from "./routes/user.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/auth", userRouter);
 
 app.get("/", (req, res) => {
   res.send("ok");
 });
 
-app.listen(4000, () => {
-  console.log(`listening on port 4000`);
-});
+const port = 4000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => console.log(`Server is listening port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
