9e0fcc1152f074016c69fb9c041e9b8d619b03fd
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2024-01-04 00:13:39 +0530

    loader

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -44,7 +44,7 @@ const HomePage = () => {
         />
       </div>
 
-      {!loading ? (
+      {loading ? (
         <p>Loading Recipes...</p>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 gap-4">
